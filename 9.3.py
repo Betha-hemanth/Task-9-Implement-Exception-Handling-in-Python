b5800b7ffa82981c18eb0d938b9d2f1de23b92a5
BETHA HEMANTH <hemanthbetha50@gmail.com>
2025-11-05 22:54:12 +0530

    push

---
+++ 9.3.py
@@ -0,0 +1,11 @@
+marks = []
+try:
+    for i in range(5):
+        m = int(input(f"Enter marks of student {i + 1}: "))
+        if m < 0 or m > 100:
+            raise ValueError("Marks should be between 0 and 100")
+        marks.append(m)
+    avg = sum(marks) / len(marks)
+    print(f"Average marks: {avg}")
+except ValueError as e:
+    print("Error:", e)
